0e464f6ac2f5c0a6950ecb1541c56bd7b0ebe6f5
andresanchezz <andrespersonaluser@gmail.com>
2025-09-14 21:39:52 -0500

    feat: Files for transaction functions

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { WalletModule } from './wallet/wallet.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { CategoryModule } from './category/category.module';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [WalletModule, CategoryModule, PrismaModule],
+  imports: [WalletModule, CategoryModule, TransactionModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
